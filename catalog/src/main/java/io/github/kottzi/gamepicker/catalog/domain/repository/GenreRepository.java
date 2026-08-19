9d8bac732d323f83a035ee78bca306bb06af13d3
Denis Levshneko <129685267+kottzi@users.noreply.github.com>
2026-08-19 12:52:43 +0300

    refactor: remove unused repo's methods (#7)

--- catalog/src/main/java/io/github/kottzi/gamepicker/catalog/domain/repository/GenreRepository.java
+++ catalog/src/main/java/io/github/kottzi/gamepicker/catalog/domain/repository/GenreRepository.java
@@ -3,9 +3,5 @@ package io.github.kottzi.gamepicker.catalog.domain.repository;
 import io.github.kottzi.gamepicker.catalog.domain.model.Genre;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface GenreRepository extends CrudRepository<Genre, Long> {
-
-    Optional<Genre> findByNameIgnoreCase(String name);
 }
